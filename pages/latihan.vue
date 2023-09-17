a4aa16e600dfdc9f8124f6b097fea20151acda75
Rivalzy <rivalds011@gmail.com>
2023-09-17 08:08:48 +0700

    init

---
+++ pages/latihan.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+    <h1>{{ count }}</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      shopNumber: 2,
+    }
+  },
+  computed: {
+    count: function () {
+      return 'The shop number is ' + this.shopNumber
+    },
+  },
+}
+</script>
